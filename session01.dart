b2b68db2ef85839b9b02f844f9cdd712ddfcdb02
VanLinh-Flutter Dev <linh39509667@gmail.com>
2022-11-28 23:38:53 +0700

    session01

---
+++ session01.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Tên bạn là gì?");
+  String name = stdin.readLineSync()!;
+
+  print("Xin chào, $name! Bạn bao nhiêu tuổi?");
+  int age = int.parse(stdin.readLineSync()!);
+
+  int yearsToHunderd = 100 - age;
+  print("$name, Bạn còn $yearsToHunderd năm nữa là Mừng thọ 100 tuổi!");
+}
